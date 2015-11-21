67f7a957e47f3ddbf13f90c6a9bdfbe2a8ed809d
Samuel Ng <sam@infinitely.io>
2015-11-21 00:57:23 -0700

    use whitespace instead of tabs

--- build.bash
+++ build.bash
@@ -1,63 +1,63 @@
 #!/bin/bash
 
 section() {
-	if [ ! -z "$1" ]; then
-		echo
-		echo "- $1"
-		echo
-	fi
+    if [ ! -z "$1" ]; then
+        echo
+        echo "- $1"
+        echo
+    fi
 }
 
 _ARCH() {
-	_MACHINET=`uname -m`
-	_ARCH=
-	if [[ "$_MACHINET" == 'x86_64' ]]; then
-		_ARCH='amd64'
-	else
-		_ARCH='386'
-	fi
-	eval "$1=$_ARCH"
+    _MACHINET=`uname -m`
+    _ARCH=
+    if [[ "$_MACHINET" == 'x86_64' ]]; then
+        _ARCH='amd64'
+    else
+        _ARCH='386'
+    fi
+    eval "$1=$_ARCH"
 }
 
 _PLATFORM() {
-	_UNAME=`uname`
-	_PLATFORM=
-	if [[ "$_UNAME" == 'Linux' ]]; then
-		_PLATFORM='linux'
-	elif [[ "$_UNAME" == 'Darwin' ]]; then
-		_PLATFORM='darwin'
-	else
-		_PLATFORM='windows'
-	fi
-	eval "$1=$_PLATFORM"
+    _UNAME=`uname`
+    _PLATFORM=
+    if [[ "$_UNAME" == 'Linux' ]]; then
+        _PLATFORM='linux'
+    elif [[ "$_UNAME" == 'Darwin' ]]; then
+        _PLATFORM='darwin'
+    else
+        _PLATFORM='windows'
+    fi
+    eval "$1=$_PLATFORM"
 }
 
 # OPTIONS: 386 amd64
 if [ -z $ARCH ]; then
-	_ARCH ARCH
+    _ARCH ARCH
 fi
 
 # OPTIONS: darwin linux windows
 if [ -z $PLATFORM ]; then
-	_PLATFORM PLATFORM
+    _PLATFORM PLATFORM
 fi
 
 section "Download packages"
 
 for DEP in `go list -json ./src/... | jq '.Imports[]'`
 do
-	DEP="${DEP%\"}"
-	DEP="${DEP#\"}"
-	if [[ "$DEP" != _* ]]; then
-		echo " Importing $DEP..."
-		eval "go get $DEP"
-	fi
+    DEP="${DEP%\"}"
+    DEP="${DEP#\"}"
+    if [[ "$DEP" != _* ]]; then
+        echo " Importing $DEP..."
+        eval "go get $DEP"
+    fi
 done
 
 section "Packing resources"
 
 if ! which "go-bindata" >/dev/null; then
-	go get -u github.com/jteeuwen/go-bindata/...
+    go get -u github.com/jteeuwen/go-bindata/...
 fi
 
 echo " # templates"
@@ -68,20 +68,20 @@ $GOPATH/bin/go-bindata -ignore=\\.gitignore -o src/_gen_assets.go src/assets
 section "Building binary"
 
 for GOOS in $PLATFORM; do
-	for GOARCH in $ARCH; do
-		echo " Building $GOOS-$GOARCH"
-		GOOS=$GOOS GOARCH=$GOARCH go build -o bin/infinitely-$GOOS-$GOARCH src/main.go
-	done
+    for GOARCH in $ARCH; do
+        echo " Building $GOOS-$GOARCH"
+        GOOS=$GOOS GOARCH=$GOARCH go build -o bin/infinitely-$GOOS-$GOARCH src/main.go
+    done
 done
 
 if [ ! -z "$1" ]; then
 
-	section "Running executable application"
+    section "Running executable application"
 
-	_PLATFORM PLATFORM
-	_ARCH ARCH
+    _PLATFORM PLATFORM
+    _ARCH ARCH
 
-	bin/infinitely-$PLATFORM-$ARCH
+    bin/infinitely-$PLATFORM-$ARCH
 
 fi
 
