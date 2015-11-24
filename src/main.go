b9cc71b02936055c77f634b5a1df570cb8afe03f
Samuel Ng <sam@infinitely.io>
2015-11-24 01:29:58 -0700

    create web server

--- src/main.go
+++ src/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-
+    "app/server"
 )
 
 func main() {
-
+    app := server.Create(server.AppOptions{
+        Asset: Asset,
+        AssetDir: AssetDir,
+	})
+	app.Run()
 }
 
