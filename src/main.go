d8b355cc10f0b0f7e050e0a20b02ae503134a59e
Samuel Ng <sam@infinitely.io>
2015-11-24 00:56:13 -0700

    remove unused code

--- src/main.go
+++ src/main.go
@@ -2,41 +2,9 @@ package main
 
 import (
 
-    "fmt"
-    "runtime"
-
-    "github.com/gin-gonic/gin"
-
-    "./routes"
-    "./ws"
-)
-
-const (
-    port = ":3000"
 )
 
 func main() {
-    configureRuntime()
-    startServer()
-}
 
-func configureRuntime() {
-    numCPU := runtime.NumCPU()
-    runtime.GOMAXPROCS(numCPU)
-    fmt.Printf("Running with %d CPUs\n", numCPU)
-}
-
-func startServer() {
-
-    router := gin.Default()
-
-    router.Static("/assets", "src/assets")
-    router.LoadHTMLGlob("src/templates/*")
-
-    router.GET("/", routes.HomeGet)
-
-    router.GET("/sync", ws.Sync);
-
-    router.Run(port)
 }
 
