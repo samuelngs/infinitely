ce928adcbdb577045d5ab942935259def7a87359
Samuel Ng <sam@infinitely.io>
2015-11-25 01:56:50 -0700

    add online count page (for testing)

--- src/app/routes/home.go
+++ src/app/routes/home.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+    "net/http"
+
     "app/server"
 
     "github.com/gin-gonic/gin"
@@ -10,6 +12,11 @@ var HomeRoute = map[string] *server.Route {
 
     "GET[/]": server.NewRoute("GET", "/", func(c *gin.Context) {
         c.HTML(200, "index.html", nil)
+    }),
+
+    "GET[/online]": server.NewRoute("GET", "/online", func(c *gin.Context) {
+        app := c.MustGet("app").(*server.App)
+        c.String(http.StatusOK, "Hello %d", app.WebSocket.Sessions.Count())
     }),
 
 }
