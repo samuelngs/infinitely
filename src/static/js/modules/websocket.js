34ecd9b87f7659cd8fb14ee9b4f6cf79c7759c7f
Samuel Ng <sam@infinitely.io>
2015-11-26 01:30:25 -0700

    extend websocket with base class

--- src/static/js/modules/websocket.js
+++ src/static/js/modules/websocket.js
@@ -1,16 +1,44 @@
 
 ;(function() {
 
-    var app = this;
+    var global = this;
 
     var WebSocket = function WebSocket(url) {
-        this.ws = new WebSocket(url);
+        App.Core.Base.call(this);
+        this.ws = new global.WebSocket(url);
+        this._listen();
     };
 
-    WebSocket.prototype = Object.create(global.App.Core.Base.prototype);
+    WebSocket.prototype = Object.create(App.Core.Base.prototype);
     WebSocket.prototype.constructor = WebSocket;
 
-    global.App.Module.WebSocket = WebSocket;
+    WebSocket.prototype._listen = function() {
+        this.ws.addEventListener('open', this._onopen.bind(this));
+        this.ws.addEventListener('close', this._onclose.bind(this));
+        this.ws.addEventListener('message', this._onmessage.bind(this));
+    };
+
+    WebSocket.prototype._onopen = function() {
+        this.emit('open');
+    };
+
+    WebSocket.prototype._onclose = function() {
+        this.emit('open');
+    };
+
+    WebSocket.prototype._onmessage = function(msg) {
+        this.emit('message', msg);
+    };
+
+    WebSocket.prototype.send = function(msg) {
+        var data = {
+            type: 'message',
+            data: msg
+        };
+        this.ws.send(JSON.stringify(data));
+    };
+
+    App.Module.WebSocket = WebSocket;
 
 }.call(this || window));
 
