304c36373c5b6b96d425cd200cf55715a51ad70f
Samuel Ng <sam@infinitely.io>
2015-11-26 16:49:51 -0700

    update websocket module, add features

--- src/static/js/modules/websocket.js
+++ src/static/js/modules/websocket.js
@@ -5,7 +5,7 @@
 
     var WebSocket = function WebSocket(url) {
         App.Core.Base.call(this);
-        this.ws = new global.WebSocket(url);
+        this.set('ws', new global.WebSocket(url));
         this._listen();
     };
 
@@ -13,17 +13,20 @@
     WebSocket.prototype.constructor = WebSocket;
 
     WebSocket.prototype._listen = function() {
-        this.ws.addEventListener('open', this._onopen.bind(this));
-        this.ws.addEventListener('close', this._onclose.bind(this));
-        this.ws.addEventListener('message', this._onmessage.bind(this));
+        this.get('ws').addEventListener('open', this._onopen.bind(this));
+        this.get('ws').addEventListener('close', this._onclose.bind(this));
+        this.get('ws').addEventListener('message', this._onmessage.bind(this));
     };
 
     WebSocket.prototype._onopen = function() {
+        this.set('cid', 0);
+        this.set('connected', true);
         this.emit('open');
     };
 
     WebSocket.prototype._onclose = function() {
-        this.emit('open');
+        this.set('connected', false);
+        this.emit('close');
     };
 
     WebSocket.prototype._onmessage = function(msg) {
@@ -31,15 +34,16 @@
     };
 
     WebSocket.prototype.send = function(msg) {
+        this.set('cid', this.get('cid') + 1);
         var data = {
-            cid: 0,
+            cid: this.get('cid'),
             type: 'publish',
             data: {
                 channel: '',
                 data: msg
             }
         };
-        this.ws.send(JSON.stringify(data));
+        this.get('ws').send(JSON.stringify(data));
     };
 
     App.Module.WebSocket = WebSocket;
