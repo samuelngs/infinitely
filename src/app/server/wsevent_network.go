314da1c3d7bfbdee2b9f474c5451043f0105e2e9
Samuel Ng <sam@infinitely.io>
2015-11-28 14:37:12 -0700

    change wsevent demo code

--- src/app/server/wsevent_network.go
+++ src/app/server/wsevent_network.go
@@ -2,9 +2,11 @@ package server
 
 var WSEventNetwork = CreateWebSocketEvent("network").AddAuth(func(s *Session) (error) {
     return nil
-}).AddEvent("test", func(m *Message, s *Session) (error) {
-    if j, err := m.Reply("testing"); err == nil {
+}).AddEvent("ping", func(m *Message, s *Session) (error) {
+    if j, err := m.Reply("pong"); err == nil {
         s.emit(1, j)
+    } else {
+        return err
     }
     return nil
 }).AddEvent("test2", func(m *Message, s *Session) (error) {
