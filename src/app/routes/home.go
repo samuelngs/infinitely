1f875dcef63e6939707acdc740b3748ac6503e3a
Samuel Ng <sam@infinitely.io>
2015-11-27 20:16:59 -0700

    remove unused route

--- src/app/routes/home.go
+++ src/app/routes/home.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-    "net/http"
-
     "app/server"
 
     "github.com/gin-gonic/gin"
@@ -11,12 +9,6 @@ import (
 var HomeRoute = []*server.Route{
     server.NewRoute("GET", "/", func(app *server.App, c *gin.Context) {
         c.HTML(200, "index.html", nil)
-    }),
-    server.NewRoute("GET", "/online", func(app *server.App, c *gin.Context) {
-        c.String(http.StatusOK, "Hello %d", app.WebSocket.Hub().Len())
-    }),
-    server.NewRoute("GET", "/test", func(app *server.App, c *gin.Context) {
-        c.String(http.StatusOK, "lol")
     }),
 }
 
